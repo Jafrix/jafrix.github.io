d0e231f442e896604cacb0697e908d686ac46ace
Andrey Veselov <lifeforglory@gmail.com>
2024-08-19 12:24:43 +0300

    style: new style

---
+++ index.js
@@ -0,0 +1,9 @@
+// ---------------------------------------TRAINING DOM-----------------------------
+const allInputs = document.querySelector('h1');
+
+allInputs.addEventListener('click', function () {
+  allInputs.style.color = "brown";
+})
+
+
+
